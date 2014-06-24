a310885f700c6c3ade97e957bd48941605debf9f
Eduardo Malherbi Martins <emalherbi@gmail.com>
2014-06-24 15:34:23 -0300

    changes curriculum.js

--- js/curriculum.js
+++ js/curriculum.js
@@ -8,18 +8,7 @@ $(document).ready(function() {
 		var zip = new JSZip();
 		zip.file("index.html", index);
 
-		var img = zip.folder("img");
-		img.file("glyphicons-halflings.png", "img/glyphicons-halflings.png", {base64: true});
-
-		console.log( '2' );
-
 		var content = zip.generate({type:"blob"});
 		saveAs(content, "curriculum.zip");    	    	
-
-		console.log( '3' );
-
-    	var index = $('html').html().trim();
-		// var blob = new Blob([html, html], {type: "application/xhtml+xml;charset=utf-8"});
-		// saveAs(blob, "curriculum.html");    	
     });
 });
