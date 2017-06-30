e500d713342d31eecbb6671bde365bd66faf6920
Eduardo Malherbi <emalherbi@gmail.com>
2017-06-30 11:37:12 -0300

    auto-generated commit

--- js/app.js
+++ js/app.js
@@ -126,4 +126,22 @@ $(document).ready(function() {
   Resume.pdf();
   // init
   EN.show();
+  //
+  toastr.options = {
+    "closeButton": false,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-bottom-center",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  };
 });
