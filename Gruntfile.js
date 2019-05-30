0c25bb7d504dacc41780e36afe38a03631e82ade
Eduardo Malherbi <emalherbi@gmail.com>
2019-05-30 08:53:46 -0300

    ajustes de identação do código

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,5 @@
 var path = require('path');
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   'use strict';
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
